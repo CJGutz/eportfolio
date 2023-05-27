90efcde3ded0f9185745785cbaeeeb6a068070f7
Carl Gützkow <cjgutzkow@gmail.com>
2023-05-27 05:34:19 +0200

    fix mobile navbar position

--- components/Base/Header.tsx
+++ components/Base/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   const [openMobileNav, setOpenMobileNav] = useState(false);
 
   return (
-    <header className="bg-transparent">
+    <header className="bg-transparent z-30">
       <nav className="container mx-auto p-6 lg:flex lg:items-center lg:justify-between">
         <div className="flex items-center justify-between">
           <div className="z-20">
@@ -40,14 +40,14 @@ const Header = () => {
           className={`${
             openMobileNav
               ? 'translate-y-0 opacity-100'
-              : 'opacity-0 -translate-y-full'
-          } absolute z-10 inset-x-0 w-full bg-transparent px-6 py-4 shadow-md transition-all duration-300 ease-in-out lg:relative lg:top-0 lg:mt-0 lg:flex lg:w-auto lg:translate-y-0 lg:items-center lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none`}
+              : 'opacity-0 -translate-y-full h-0'
+          } inset-x-0 w-full bg-transparent px-6 py-4 shadow-md transition-all duration-200 ease-in-out lg:relative lg:top-0 lg:mt-0 lg:flex lg:w-auto lg:translate-y-0 lg:items-center lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none`}
         >
           <div className="flex flex-col space-y-4 lg:mt-0 lg:flex-row lg:space-y-0">
             {links.map(({ name, href }) => (
               <Link
                 key={name}
-                className="text-sm font-medium text-gray-700 hover:text-blue-500 lg:mx-4 lg:my-0 lg:mvn-button"
+                className="text-sm font-medium text-gray-700 hover:text-blue-500 lg:mx-4 lg:mvn-button"
                 href={href}
               >
                 {name}
