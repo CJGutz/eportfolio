47f009a99856c98853de49e31ea243f94d17673b
Carl Gützkow <cjgutzkow@gmail.com>
2023-06-02 22:42:01 +0200

    add more experiences

--- data/experiences.ts
+++ data/experiences.ts
@@ -11,14 +11,35 @@ export type ExperienceListNodeType = {
 export const ExperiencesListNode: Map<string, ExperienceListNodeType> = new Map(
   [
     [
+      'quantum-algorithms',
+      {
+        title: 'Trying out Quantum Computing',
+        shortDescription: 'Followed a course on Quantum Computing and tried out some algorithms with the Qiskit Python library.',
+        dateStart: new Date(2021, 6, 1),
+        sourceLink: 'https://github.com/CJGutz/Quantum-Algorithms',
+      },
+    ],
+    [
       'index',
       {
-        title: 'Index',
+        title: 'Programmer with Index',
         shortDescription: 'Worked as a Back-end developer for index.',
         dateStart: new Date(2021, 8, 1),
         dateEnd: new Date(2022, 5, 1),
         demoLink: 'https://tihlde.org',
         sourceLink: 'https://github.com/TIHLDE/Lepton',
+      },
+    ],
+    [
+      'hackerspace-devops',
+      {
+        title: 'DevOps Team Leader - Hackerspace',
+        shortDescription:
+          'For a year I managed the DevOps team at Hackerspace NTNU. I had responsibility for the development lifecycle, server infrastructure and the team\'s well-being.',
+        dateStart: new Date(2022, 2, 1),
+        dateEnd: new Date(2023, 2, 1),
+        demoLink: 'https://hackerspace-ntnu.no',
+        sourceLink: 'https://github.com/hackerspace-ntnu',
       },
     ],
     [
@@ -33,25 +54,15 @@ export const ExperiencesListNode: Map<string, ExperienceListNodeType> = new Map(
       },
     ],
     [
-      'erfe',
+      'hackerspace-deputy',
       {
-        title: 'Ei Solutions',
+        title: 'Deputy Commander - Hackerspace',
         shortDescription:
-          'Created the Back-end for a automatic EU taxonomy reporter. I also created the landing page.',
-        dateStart: new Date(2022, 8, 1),
-        dateEnd: new Date(Date.now()),
-        demoLink: 'https://eisolutions.no',
-      },
-    ],
-    [
-      'eisutions',
-      {
-        title: 'Ei Solutions',
-        shortDescription:
-          'Created the Back-end for a automatic EU taxonomy reporter. I also created the landing page.',
-        dateStart: new Date(2022, 8, 1),
-        dateEnd: new Date(Date.now()),
-        demoLink: 'https://eisolutions.no',
+          'The deputy commander, together with the lead and the financial manager, have the responsibility to administer the organization. This includes having equipment available for students, organizing events like the general assembly, and creating an environment for students to learn.',
+        dateStart: new Date(2023, 2, 1),
+        dateEnd: new Date(2024, 2, 1),
+        demoLink: 'https://hackerspace-ntnu.no',
+        sourceLink: 'https://github.com/hackerspace-ntnu',
       },
     ],
   ]
