90361f1f55e18d5916a4e9547c7a3052ff9114ba
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-03-03 15:30:21 +0100

    add stats from olros

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { stats } from 'utils/stats';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    stats.pageview();
+  }, [router.pathname])
   return <Component {...pageProps} />;
 }
---
+++ utils/stats.ts
@@ -0,0 +1,7 @@
+import { Stats } from '@olros/stats'
+
+const team = "CJGutz"
+const project = "eportfolio"
+
+export const stats = Stats({team: team, project: project})
+
